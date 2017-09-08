2fa68fd54e68b2684df0a6c94f1042b1d9728c3b
wkmeijer <meijer.wilko@gmail.com>
2017-09-08 15:07:13 +0200

    setup android project

---
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/MainActivity.java
@@ -0,0 +1,13 @@
+package nl.tudelft.cs4160.trustchain_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
