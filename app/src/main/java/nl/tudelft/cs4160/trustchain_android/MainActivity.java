88bd2a3c98c622595add44a23e575ef5f343f1b0
wkmeijer <meijer.wilko@gmail.com>
2017-09-11 22:01:42 +0200

    external IP address is displayed on app startup

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/MainActivity.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/MainActivity.java
@@ -1,7 +1,15 @@
 package nl.tudelft.cs4160.trustchain_android;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        updateIP();
+    }
+
+    /**
+     * Updates the IP address textfield to the given IP address.
+     */
+    public void updateIPField(String ipAddress) {
+        TextView myIp = (TextView) findViewById(R.id.my_ip);
+        myIp.setText(ipAddress);
+        System.out.println("IP ADDRESS: " + ipAddress);
+    }
+
+    /**
+     * Finds the external IP address of this device by making an API call to https://www.ipify.org/.
+     * The networking runs on a separate thread.
+     * @return a string representation of the device's external IP address
+     */
+    public void updateIP() {
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try (java.util.Scanner s = new java.util.Scanner(new java.net.URL("https://api.ipify.org").openStream(), "UTF-8").useDelimiter("\\A")) {
+                    final String ip = s.next();
+                    // new thread to handle UI updates
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            updateIPField(ip);
+                        }
+                    });
+                } catch (java.io.IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    /**
+     * Finds the local IP address of this device, loops trough network interfaces in order to find it.
+     * The address that is not a loopback address is the IP of the device.
+     * @return a string representation of the device's IP address
+     * TODO: remove when we end up not using any local addresses
+     */
+    public String getLocalIPAddress() {
+        try {
+            List<NetworkInterface> netInterfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface netInt : netInterfaces) {
+                List<InetAddress> addresses = Collections.list(netInt.getInetAddresses());
+                for (InetAddress addr : addresses) {
+                    if(!addr.isLoopbackAddress()) {
+                        return addr.getHostAddress();
+                    }
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
