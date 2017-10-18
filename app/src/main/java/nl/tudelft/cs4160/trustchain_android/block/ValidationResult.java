c12a5b813e5cc68468a20daf9ed4d51da0c2b80b
meijer <wkmeijer@users.noreply.github.com>
2017-10-18 16:22:28 +0200

    created validation result object

---
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/block/ValidationResult.java
@@ -0,0 +1,96 @@
+package nl.tudelft.cs4160.trustchain_android.block;
+
+/**
+ * Created by wkmeijer on 18-10-17.
+ */
+
+public class ValidationResult {
+    int status;
+
+    final static int VALID = 0;
+    final static int PARTIAL = 1;
+    final static int PARTIAL_NEXT = 2;
+    final static int PARTIAL_PREVIOUS = 3;
+    final static int NO_INFO = 4;
+    final static int INVALID = 5;
+
+    /**
+     * Constructor, we always start out thinking everything is fine.
+     */
+    public ValidationResult() {
+        status = VALID;
+    }
+
+    /**
+     * The block does not violate any rules, but there are gaps between this and previous or next
+     * blocks, or it does not have previous or next blocks.
+     * @return
+     */
+    public ValidationResult setPartial(){
+        this.status = PARTIAL;
+        return this;
+    }
+
+    /**
+     * The block does not violate any rules, but there are gaps between this and the next block
+     * or no next blocks.
+     * @return
+     */
+    public ValidationResult setPartialNext(){
+        this.status = PARTIAL_NEXT;
+        return this;
+    }
+
+    /**
+     * The block does not violate any rules, but there are gaps between this and the previous block
+     * or no previous blocks.
+     * @return
+     */
+    public ValidationResult setPartialPrevious(){
+        this.status = PARTIAL_PREVIOUS;
+        return this;
+    }
+
+    /**
+     * There are no blocks (previous or next) to validate against.
+     * @return
+     */
+    public ValidationResult setNoInfo(){
+        this.status = NO_INFO;
+        return this;
+    }
+
+    /**
+     * The block violates at least one validation rule.
+     * @return
+     */
+    public ValidationResult setInvalid(){
+        this.status = INVALID;
+        return this;
+    }
+
+    /**
+     * Creates a string representation of the ValidationResult, based on its status.
+     * @return String representation of this ValidationResult
+     */
+    public String toString() {
+        String res = "<ValidationResult: ";
+        switch(status){
+            case VALID: res += "VALID";
+                break;
+            case PARTIAL: res += "PARTIAL";
+                break;
+            case PARTIAL_NEXT: res += "PARTIAL_NEXT";
+                break;
+            case PARTIAL_PREVIOUS: res += "PARTIAL_PREVIOUS";
+                break;
+            case NO_INFO: res += "NO_INFO";
+                break;
+            case INVALID: res += "INVALID";
+                break;
+        }
+
+        return res + ">";
+    }
+
+}
