91ff9bcbb5a7598ce9158cec2bddb7736c4fd8e7
klikooo <rico12978@hotmail.com>
2017-11-10 14:22:54 +0100

    Send block via bluetooth

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/connection/BluetoothActivity.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/connection/BluetoothActivity.java
@@ -1,38 +1,21 @@
 package nl.tudelft.cs4160.trustchain_android.connection;
 
-import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.ParcelUuid;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Set;
 import java.util.UUID;
 
 import nl.tudelft.cs4160.trustchain_android.R;
@@ -46,7 +29,7 @@ public class BluetoothActivity extends AppCompatActivity {
     private final static String TAG = BluetoothActivity.class.getName();
 
     private BluetoothAdapter btAdapter;
-    private UUID myUUID;
+    private final UUID myUUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");;
 
     private ListView listPairedDevices;
 
@@ -57,7 +40,8 @@ public class BluetoothActivity extends AppCompatActivity {
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
             BluetoothDevice device = (BluetoothDevice) adapterView.getItemAtPosition(i);
             Log.e(TAG, "pressed " + device.getName() + "\nUUID: " +device.getUuids()[0].getUuid());
-            //start new tread
+
+            //start new tread that sends a block to the other party
             new ConnectThread(device).start();
         }
     };
@@ -75,6 +59,7 @@ public class BluetoothActivity extends AppCompatActivity {
 
     public void init() {
         btAdapter = BluetoothAdapter.getDefaultAdapter();
+        //TODO: show error messages
         if (btAdapter == null) {
             Log.w(TAG, "Bluetooth is not supported");
             return;
@@ -89,27 +74,10 @@ public class BluetoothActivity extends AppCompatActivity {
         listPairedDevices.setAdapter(new DeviceAdapter(getApplicationContext(), btAdapter.getBondedDevices()));
         listPairedDevices.setOnItemClickListener(itemClickListener);
 
-        setMyUUID();
 
 
         acceptThread = new AcceptThread();
         acceptThread.start();
-    }
-
-    private void setMyUUID() {
-//        try {
-//            Method getUUIDsMethod = BluetoothAdapter.class.getDeclaredMethod("getUuids", null);
-//            ParcelUuid[] dUUIDs = (ParcelUuid[]) getUUIDsMethod.invoke(btAdapter, null);
-//            myUUID =dUUIDs[dUUIDs.length-1].getUuid();
-            myUUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
-            Log.i(TAG, "Set UUID to: " + myUUID.toString());
-//        } catch (NoSuchMethodException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        } catch (InvocationTargetException e) {
-//            e.printStackTrace();
-//        }
     }
 
 
@@ -138,7 +106,7 @@ public class BluetoothActivity extends AppCompatActivity {
         }
 
         public void run() {
-            BluetoothSocket socket = null;
+            BluetoothSocket socket;
             // Keep listening until exception occurs or a socket is returned.
             while (true) {
                 try {
@@ -154,23 +122,19 @@ public class BluetoothActivity extends AppCompatActivity {
                     //TODO: read something
                     try {
                         Log.i(TAG, "created a server");
-//                        while(true) {
-                            /*try {
-                                MessageProto.Message message= MessageProto.Message.parseFrom(socket.getInputStream());
+                        while(true) {
+                            try {
+                                Log.e(TAG, "starting reading");
+                                MessageProto.Message message= MessageProto.Message.parseDelimitedFrom(socket.getInputStream());
                                 if(message == null) {
                                     break;
                                 }
+                                Log.e(TAG, "done reading");
+                                Log.e(TAG, "link sequence: " + message.getHalfBlock().getLinkSequenceNumber());
                             } catch(InvalidProtocolBufferException e) {
                                 Log.e(TAG, "invalid message received: " + e.getMessage());
-                            }*/
-                            byte[] buffer = new byte[1024];
-                            socket.getInputStream().read(buffer);
-                            if(buffer[0] == 1 && buffer[1] == 2 && buffer[2] == 3) {
-                                Log.i(TAG, "Received message");
-                            } else {
-                                Log.e(TAG, "Received wrong message");
                             }
-  //                      }
+                        }
 
 
                         mmServerSocket.close();
@@ -242,10 +206,7 @@ public class BluetoothActivity extends AppCompatActivity {
                         .setSequenceNumber(1).setLinkSequenceNumber(2);
                 MessageProto.TrustChainBlock block = builder.build();
                 MessageProto.Message msg = newBuilder().setHalfBlock(block).build();
-                //MessageProto.TrustChainBlock msg = newBuilder().setHalfBlock(block).build();
-                os.write(new byte[] { 0x01, 0x02, 0x03});
-                //msg.writeTo(os);
-                //os.write(msg.toByteArray());
+                msg.writeDelimitedTo(mmSocket.getOutputStream());
                 Log.e(TAG, "send message!");
             } catch (IOException e) {
                 e.printStackTrace();
