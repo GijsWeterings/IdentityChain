38e1d25fcc48fc112a3954a7fe163ff64b8eb829
klikooo <rico12978@hotmail.com>
2017-11-10 18:58:09 +0100

    Remove functions from Server to Communication

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/main/Server.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/main/Server.java
@@ -1,7 +1,6 @@
 package nl.tudelft.cs4160.trustchain_android.main;
 
 import android.util.Log;
-import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -12,8 +11,6 @@ import java.net.Socket;
 import java.util.Arrays;
 
 import nl.tudelft.cs4160.trustchain_android.Peer;
-import nl.tudelft.cs4160.trustchain_android.R;
-import nl.tudelft.cs4160.trustchain_android.Util.Key;
 import nl.tudelft.cs4160.trustchain_android.block.TrustChainBlock;
 import nl.tudelft.cs4160.trustchain_android.block.ValidationResult;
 import nl.tudelft.cs4160.trustchain_android.database.TrustChainDBHelper;
@@ -78,39 +75,7 @@ class Server {
 
                     // We have received a message, this could be either a crawl request or a halfblock
                     MessageProto.Message message = MessageProto.Message.parseFrom(socket.getInputStream());
-
-                    MessageProto.TrustChainBlock block = message.getHalfBlock();
-                    MessageProto.CrawlRequest crawlRequest = message.getCrawlRequest();
-
-                    // In case we received a halfblock
-                    if(block.getPublicKey().size() > 0 && crawlRequest.getPublicKey().size() == 0) {
-                        count++;
-                        messageLog += "block received from: " + socket.getInetAddress() + ":"
-                                + socket.getPort() + "\n"
-                                + TrustChainBlock.toShortString(block);
-
-                        listener.updateLog("\n  Server: " + messageLog);
-
-                        SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(
-                                socket, count);
-                        socketServerReplyThread.run();
-
-                        synchronizedReceivedHalfBlock(socket.getInetAddress(), socket.getPort(), block);
-                    }
-
-                    // In case we received a crawlrequest
-                    if(block.getPublicKey().size() == 0 && crawlRequest.getPublicKey().size() > 0) {
-                        count++;
-                        messageLog += "crawlrequest received from: " + socket.getInetAddress() + ":"
-                                + socket.getPort();
-                        listener.updateLog("\n  Server: " + messageLog);
-                        SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(
-                                socket, count);
-                        socketServerReplyThread.run();
-
-                        communication.receivedCrawlRequest(socket.getInetAddress(),
-                                socket.getPort(), crawlRequest);
-                    }
+                    communication.receiveMessage(message, socket.getInetAddress(), socket.getPort());
 
                 }
             } catch (IOException e) {
@@ -153,74 +118,4 @@ class Server {
         }
 
     }
-
-    /**
-     * A half block was send to us and received by us. Someone wants this peer to create the other half
-     * and send it back. This method handles that 'request'.
-     *  - Checks if the block is valid and puts it in the database if not invalid.
-     *  - Checks if the block is addressed to me.
-     *  - Determines if we should sign the block
-     *  - Check if block matches with its previous block, send crawl request if more information is needed
-     */
-    public void synchronizedReceivedHalfBlock(InetAddress address, int port, MessageProto.TrustChainBlock block) {
-        TrustChainDBHelper dbHelper = communication.getDbHelper();
-        Peer peer = new Peer(block.getPublicKey().toByteArray(), address.getHostAddress(), DEFAULT_PORT);
-        Log.i(TAG, "Received half block from peer with IP: " + peer.getIpAddress() + ":" + peer.getPort() +
-            " and public key: " + bytesToHex(peer.getPublicKey()));
-
-        communication.addNewPublicKey(peer);
-
-        ValidationResult validation;
-        try {
-            validation = validate(block,dbHelper);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-
-        Log.i(TAG,"Received block validation result " + validation.toString() + "("
-                + TrustChainBlock.toString(block) + ")");
-
-        if(validation.getStatus() == ValidationResult.INVALID) {
-            for(String error: validation.getErrors()) {
-                Log.e(TAG, "Validation error: " + error);
-            }
-            return;
-        } else {
-            insertInDB(block,dbHelper.getWritableDatabase());
-        }
-
-        byte[] pk = communication.getMyPublicKey();
-        // check if addressed to me and if we did not sign it already, if so: do nothing.
-        if(block.getLinkSequenceNumber() != UNKNOWN_SEQ ||
-                !Arrays.equals(block.getLinkPublicKey().toByteArray(), pk) ||
-                null != getBlock(dbHelper.getReadableDatabase(),
-                            block.getLinkPublicKey().toByteArray(),
-                            block.getLinkSequenceNumber())) {
-            Log.e(TAG,"Received block not addressed to me or already signed by me.");
-            return;
-        }
-
-        // determine if we should sign the block, if not: do nothing
-        if(!Communication.shouldSign(block)) {
-            Log.e(TAG,"Will not sign received block.");
-            return;
-        }
-
-        // check if block matches up with its previous block
-        // At this point gaps cannot be tolerated. If we detect a gap we send crawl requests to fill
-        // the gap and delay the method until the gap is filled.
-        // Note that this code does not cover the scenario where we obtain this block indirectly,
-        // because the code does nothing with this block after the crawlRequest was received.
-        if(validation.getStatus() == PARTIAL_PREVIOUS || validation.getStatus() == PARTIAL ||
-                validation.getStatus() == NO_INFO) {
-            Log.e(TAG, "Request block could not be validated sufficiently, requested crawler. " +
-                    validation.toString());
-            // send a crawl request, requesting the last 5 blocks before the received halfblock (if available) of the peer
-            communication.sendCrawlRequest(peer,block.getPublicKey().toByteArray(),Math.max(GENESIS_SEQ,block.getSequenceNumber()-5));
-        } else {
-            communication.signBlock(peer, block);
-        }
-    }
-
 }
