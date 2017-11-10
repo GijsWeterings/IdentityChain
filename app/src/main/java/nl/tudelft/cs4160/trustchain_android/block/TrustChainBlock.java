984d8bc7a76c5531f8f57cb907b7449b9abcb661
wkmeijer <meijer.wilko@gmail.com>
2017-11-10 22:44:00 +0100

    improved representation of crawl request in log

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/block/TrustChainBlock.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/block/TrustChainBlock.java
@@ -23,6 +23,7 @@ import nl.tudelft.cs4160.trustchain_android.database.TrustChainDBHelper;
 import nl.tudelft.cs4160.trustchain_android.message.MessageProto;
 
 import static nl.tudelft.cs4160.trustchain_android.Peer.bytesToHex;
+import static nl.tudelft.cs4160.trustchain_android.Util.Util.ellipsize;
 
 /**
  * Created by meijer on 20-9-17.
@@ -607,13 +608,13 @@ public class TrustChainBlock {
      */
     public static String toString(MessageProto.TrustChainBlock block){
         String res = "Trustchainblock: {\n";
-        res += "Public key: " + bytesToHex(block.getPublicKey().toByteArray()) + "\n";
-        res += "Sequence Number: " + block.getSequenceNumber() + "\n";
-        res += "Link Public Key: " + bytesToHex(block.getLinkPublicKey().toByteArray()) + "\n";
-        res += "Link Sequence Number: " + block.getLinkSequenceNumber() + "\n";
-        res += "Previous Hash: " + bytesToHex(block.getPreviousHash().toByteArray()) + "\n";
-        res += "Signature: " + bytesToHex(block.getSignature().toByteArray()) + "\n";
-        res += "Transaction: \n" + block.getTransaction().toStringUtf8() + "\n";
+        res += "\tPublic key: " + bytesToHex(block.getPublicKey().toByteArray()) + "\n";
+        res += "\tSequence Number: " + block.getSequenceNumber() + "\n";
+        res += "\tLink Public Key: " + bytesToHex(block.getLinkPublicKey().toByteArray()) + "\n";
+        res += "\tLink Sequence Number: " + block.getLinkSequenceNumber() + "\n";
+        res += "\tPrevious Hash: " + bytesToHex(block.getPreviousHash().toByteArray()) + "\n";
+        res += "\tSignature: " + bytesToHex(block.getSignature().toByteArray()) + "\n";
+        res += "\tTransaction: \n" + block.getTransaction().toStringUtf8() + "\n";
         res += "}";
         return res;
     }
@@ -626,29 +627,25 @@ public class TrustChainBlock {
      */
     public static String toShortString(MessageProto.TrustChainBlock block){
         String res = "Trustchainblock: {\n";
-        res += "Public key: " + pubKeyToString(block.getPublicKey().toByteArray()) + "\n";
-        res += "Sequence Number: " + block.getSequenceNumber() + "\n";
-        res += "Link Public Key: " + pubKeyToString(block.getLinkPublicKey().toByteArray()) + "\n";
-        res += "Link Sequence Number: " + block.getLinkSequenceNumber() + "\n";
+        res += "\tPublic key: " + pubKeyToString(block.getPublicKey().toByteArray(),32) + "\n";
+        res += "\tSequence Number: " + block.getSequenceNumber() + "\n";
+        res += "\tLink Public Key: " + pubKeyToString(block.getLinkPublicKey().toByteArray(),32) + "\n";
+        res += "\tLink Sequence Number: " + block.getLinkSequenceNumber() + "\n";
         res += "}";
         return res;
     }
 
     /**
      * Helper method for toString method of TrustChainBlock. Creates a representation of a public key
-     * representing a maximum of 64 bytes.
+     * with a maximum length.
      * @param pubKey
+     * @param maxLength
      * @return
      */
-    private static String pubKeyToString(byte[] pubKey){
+    public static String pubKeyToString(byte[] pubKey, int maxLength){
         String res;
         int length = pubKey.length;
-        if(length > 64) {
-            res = bytesToHex(Arrays.copyOfRange(pubKey,0,32)) + "(...)"
-                    + bytesToHex(Arrays.copyOfRange(pubKey,length-33,length-1));
-        } else {
-            res = bytesToHex(pubKey);
-        }
+        res = ellipsize(bytesToHex(pubKey), maxLength);
         res += " (size: " + length + ")";
         return res;
     }
