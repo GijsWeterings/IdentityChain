85220bcee21ded68f445850d8d968568381d07ac
Recognition2 <gkh998@gmail.com>
2017-12-06 11:54:29 +0100

    Add Generator start

--- zeroknowledgeproof/src/main/java/com/example/zeroknowledgeproof/RangeProof.kt
+++ zeroknowledgeproof/src/main/java/com/example/zeroknowledgeproof/RangeProof.kt
@@ -1,5 +1,18 @@
 package com.example.zeroknowledgeproof
 
-class RangeProof {
+// An implementation based on An Efficient Range Proof Scheme by Kun Peng and Feng Bao
 
+class RangeProof () {
+    /**
+     * Generate a proof that a given number is in a range
+     */
+    fun Generator (m: Int, lowerBound: Int, upperBound: Int) : String {
+        // Prove that boundedNumber is between lower and upper bound
+        if ((m - lowerBound + 1)*(upperBound - m + 1) <= 0) {
+            throw IllegalArgumentException("Cannot generate proof, because this is just NOT TRUE")
+        }
+
+
+
+    }
 }
