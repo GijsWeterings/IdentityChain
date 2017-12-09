465789c3c9aa80f3280ee6a587ec92e27bd09539
Recognition2 <gkh998@gmail.com>
2017-12-09 23:39:06 +0100

    Add NumberProof implementation, include tests. Does work,  not sure how to use as of yet.
    The entire exchange between prover and verifier goes via the TrustedParty singleton in this implementation, but this can be changed easily.
    
    Tests for Verifier are added and confirmed working.
    Tests for Prover are absent, hard to figure out.
    Test coverage still shows 0, not sure how to fix.

---
+++ zeroknowledgeproof/src/main/java/com/zeroknowledgeproof/numberProof/Verifier.kt
@@ -0,0 +1,29 @@
+package com.zeroknowledgeproof.numberProof
+
+import java.math.BigInteger
+import java.security.SecureRandom
+
+class Verifier (givenN: BigInteger) {
+    private val N = givenN
+    private val rand = SecureRandom()
+
+    fun requestChallenge() : Challenge = rand.nextBoolean()
+
+    /**
+     *
+     */
+    fun verify(x: BigInteger, y: BigInteger, publicKey: BigInteger, challenge: Challenge): Boolean {
+        println("x = " + x.toString(10))
+        println("y = " + y.toString(10))
+        println("pubkey = " + publicKey.toString(10))
+        println("challenge = " + challenge)
+        println("N = " + N.toString(10))
+        if (y == BigInteger.ZERO) return false;
+
+        return if (challenge) {
+            y.times(y).mod(N) == x.times(publicKey).mod(N)
+        } else {
+            y.times(y).mod(N) == x
+        }
+    }
+}
