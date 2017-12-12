74ded98d9af8e836ebf79595cc63a3a619bfddd8
Gregory Kevin Hill <gkh998@gmail.com>
2017-12-12 15:46:00 +0100

    Fixed proof that 2 commitments hide the same secret

--- zeroknowledgeproof/src/main/java/com/zeroknowledgeproof/rangeProof/RangeProofTrustedParty.kt
+++ zeroknowledgeproof/src/main/java/com/zeroknowledgeproof/rangeProof/RangeProofTrustedParty.kt
@@ -2,16 +2,16 @@ package com.zeroknowledgeproof.rangeProof
 
 import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertTrue
+import org.spongycastle.asn1.x500.style.RFC4519Style.c
 import java.math.BigInteger
 import java.math.BigInteger.ONE
 import java.math.BigInteger.ZERO
+import java.security.MessageDigest
 import java.security.SecureRandom
+import java.security.Security
 import kotlin.system.exitProcess
 
-
 // An implementation based on An Efficient Range Proof Scheme by Kun Peng and Feng Bao
-
-
 
 object RangeProofTrustedParty {
     /**
@@ -94,6 +94,7 @@ object RangeProofTrustedParty {
         toPublish.cPrime = cPrime
         println(">STEP 2 COMPLETE")
 
+
         // Step 3: Choose w from Zk2 - {0}, and r'' from Zk2
         val w = generateRandomInterval(ONE, k2) // Lower bound is 1
         val rDPrime = generateRandomInterval(ZERO, k2)
@@ -168,6 +169,7 @@ object RangeProofTrustedParty {
         return rpverifier.verify(toPublish)
     }
 
+
     private fun proveCommittedNumberIsSquare() {
         proveTwoCommittedIntegersAreEqual()
 //        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -195,50 +197,55 @@ object RangeProofTrustedParty {
         return arrayOf(m1, m2, m4)
     }
 
+    data class IntEqualResult(
+        val stub: BigInteger
+    )
+
+
     /**
      * @param N: Large composite, factorization unknown
      * @param g1: Element of large order in Zn*
      */
-    private fun proveTwoCommittedIntegersAreEqual(){ return}
-//    private fun proveTwoCommittedIntegersAreEqual(committedNum: BigInteger, r1 :BigInteger, r2: BigInteger, N: BigInteger, g1 : BigInteger, h1: BigInteger, g2: BigInteger, h2: BigInteger) : commitmentReturn {
-////        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//
-//        // Init security parameters
-//        val b = BigInteger(512, rand)
-//        val t = 80
-//        val l = 40
-//        val s1 = 40
-//        val s2 = 552
-//
-//        // Hash function H should output 2t-bit strings.
-//
-//
-//        // Step 1: Generate random numbers.
-//        val w = generateRandomInterval(ONE, TWO.pow(l+t) * b - ONE)
-//        val eta1 = generateRandomInterval(ONE, TWO.pow(l+t+s1) * N - ONE)
-//        val eta2 = generateRandomInterval(ONE, TWO.pow(l+t+s2) * N - ONE)
-//
-//        // Generate two commitments
-//        val W1 = g1.modPow(w, N) * h1.modPow(eta1, N) % N
-//        val W1ShouldBe = g1.modPow(w, N).times(h1.modPow(eta1, N)).mod(N)
-//        assert(W1 == W1ShouldBe)
-//
-//        val W2 = g2.modPow(w, N).times(h2.modPow(eta2, N)).mod(N)
-//
-//        // Step 2: Use the hash function.
-//        // TODO Implement hash function
-//        val c = W1 + W2
-//
-//        // Step 3: Compute verification parameters
-//        val D = w + c * committedNum;
-//        val D1 = eta1 + c*r1
-//        val D2 = eta2 + c*r2
-//
-//        val E = g1.modPow(committedNum ,N).times(h1.modPow(r1, N)).mod(N)
-//        val F = g2.modPow(committedNum ,N).times(h2.modPow(r2, N)).mod(N)
-//
-//        return commitmentReturn(c, D, D1, D2, E, F)
-//    }
+    private fun proveTwoCommittedIntegersAreEqual(committedNum: BigInteger, r1 :BigInteger, r2: BigInteger, N: BigInteger, g1 : BigInteger, h1: BigInteger, g2: BigInteger, h2: BigInteger) : commitmentReturn {
+
+        // Init security parameters
+        val b = BigInteger(512, rand)
+        val t = 80
+        val l = 40
+        val s1 = 40
+        val s2 = 552
+
+        // Hash function H should output 2t-bit strings.
+
+
+        // Step 1: Generate random numbers.
+        val w = generateRandomInterval(ONE, TWO.pow(l+t) * b - ONE)
+        val eta1 = generateRandomInterval(ONE, TWO.pow(l+t+s1) * N - ONE)
+        val eta2 = generateRandomInterval(ONE, TWO.pow(l+t+s2) * N - ONE)
+
+        // Generate two commitments
+        val W1 = g1.modPow(w, N) * h1.modPow(eta1, N) % N
+        val W1ShouldBe = g1.modPow(w, N).times(h1.modPow(eta1, N)).mod(N)
+        assert(W1 == W1ShouldBe)
+
+        val W2 = g2.modPow(w, N).times(h2.modPow(eta2, N)).mod(N)
+
+        // Step 2: Use the hash function.
+        Security.insertProviderAt(org.spongycastle.jce.provider.BouncyCastleProvider(), 1)
+        val messageDigest = MessageDigest.getInstance("SHA-512")
+        // Cast W1 and W2 to byte array's, append them, hash that, and cast it back to a BigInteger
+        val c = BigInteger(messageDigest.digest(W1.toByteArray() + W2.toByteArray()))
+
+        // Step 3: Compute verification parameters
+        val D = w + c * committedNum;
+        val D1 = eta1 + c*r1
+        val D2 = eta2 + c*r2
+
+        val E = g1.modPow(committedNum ,N).times(h1.modPow(r1, N)).mod(N)
+        val F = g2.modPow(committedNum ,N).times(h2.modPow(r2, N)).mod(N)
+
+        return commitmentReturn(c, D, D1, D2, E, F)
+    }
 
     data class commitmentReturn (
             val c : BigInteger,
@@ -360,4 +367,10 @@ fun pow(base: BigInteger, exponent: BigInteger): BigInteger {
         exponent = exponent.shiftRight(1)
     }
     return result
+}
+
+fun hash(): ByteArray {
+    Security.insertProviderAt(org.spongycastle.jce.provider.BouncyCastleProvider(), 1)
+    val messageDigest = MessageDigest.getInstance("SHA-512")
+    return messageDigest.digest(someBytes)
 }
