c17eac94c590de8ad0bbd1bd9532fbd7bb980df1
LiamClark <L.A.Clark@student.tudelft.nl>
2018-01-23 16:54:08 +0100

    Verify threading operator

--- app/src/main/java/nl/tudelft/cs4160/identitychain/grpc/Operators.kt
+++ app/src/main/java/nl/tudelft/cs4160/identitychain/grpc/Operators.kt
@@ -28,6 +28,13 @@ fun <A> ListenableFuture<A>.guavaAsSingle(scheduler: Scheduler): Single<A> {
     }
 }
 
+/**
+ * This is a work around for the network on main thread exception.
+ * Even though the we use the async version of grpc, it does some network call
+ * that sometimes triggers the network on main thread exception as setup.
+ *
+ * This runs that code in the back ground aswell
+ */
 fun <T> startNetworkOnComputation(f: () -> Single<T>): Single<T> {
     return Single.defer{
         f()
